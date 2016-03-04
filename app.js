860ccc6461d9389bb68bf431100a0c7ef70e2976
Chaitanya Khanna <c2khanna@uwaterloo.ca>
2016-03-04 01:50:46 -0500

    almost complete

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 angular.module('myApp', ['ngMaterial'])
 .controller('BodyController', function($element) {
 	var vm = this;
+	vm.questions = questions;
+	vm.questionIndex = 0;
 	vm.searchText = "";
 	vm.nextButton = false;
 	vm.querySearch = function (query) {
@@ -8,6 +10,43 @@ angular.module('myApp', ['ngMaterial'])
       		return c.toLowerCase().indexOf(query.toLowerCase()) >= 0;
       	});
         return results;
+    }
+    vm.onNextButtonClick = function() {
+    	$element.find(".city").css({
+    		opacity: 0,
+    		display: "none"
+    	});
+    	$element.find(".questions").css({
+    		opacity: 1,
+    		display: "block"
+    	});
+    }
+
+    vm.onYesButtonClick = function(){
+    	if(vm.questionIndex == vm.questions.length - 1) {
+    		$element.find(".questions").css({
+	    		opacity: 0,
+	    		display: "none"
+    		});
+    		$element.find(".success").css({
+	    		opacity: 1,
+	    		display: "block"
+    		});
+    	}
+    	else {
+    		vm.questionIndex++;
+    	}
+    }
+
+    vm.onNoButtonClick = function(){
+    	$element.find(".questions").css({
+	    		opacity: 0,
+	    		display: "none"
+    		});
+    		$element.find(".fail").css({
+	    		opacity: 1,
+	    		display: "block"
+    		});
     }
 
     vm.selectedItemChange = function (item){
---
+++ questions.js
@@ -0,0 +1,8 @@
+var questions = ["Are you at least 21 years old?",
+"Do you have a full drivers license?",
+"Do you have a car that is not more than 10 years old?",
+"Do you have personal insurance for your vehicle?",
+"Do you have registration for the vehicle?",
+"Does your vehicle have at least 4 doors?",
+"Does your vehicle have at least 5 seats?"
+];
